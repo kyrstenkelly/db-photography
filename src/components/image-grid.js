cb11864f78d5265711a4459e03d3648bd8c5817c
Kyrsten Kelly <kyrsten.kelly@gmail.com>
2017-12-04 22:45:01 -0600

    Image hovers working.

--- src/components/image-grid.js
+++ src/components/image-grid.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component }   from 'react';
+import Image                  from './image';
 
-import Image from './image';
+import sanDiego       from '../images/san-diego.jpg';
+import sanDiego2      from '../images/san-diego-2.jpg';
+import ciara          from '../images/ciara.jpg';
+import ciara2         from '../images/ciara-2.jpg';
+import kelsey         from '../images/kelsey.jpg';
+import kelseyGarret   from '../images/kelsey-garret.jpg';
+import thailand       from '../images/thailand.jpg';
+import thailand2      from '../images/thailand-2.jpg';
 
 class ImageGrid extends Component {
   render() {
     return (
-      <div>
-        <Image />
+      <div className='grid'>
+        <Image src={sanDiego} />
+        <Image src={sanDiego2} />
+        <Image src={ciara} />
+        <Image src={ciara2} />
+        <Image src={kelsey} />
+        <Image src={kelseyGarret} />
+        <Image src={thailand} />
+        <Image src={thailand2} />
       </div>
     );
   }
--- src/components/image.js
+++ src/components/image.js
@@ -1,14 +1,57 @@
 import React, { Component } from 'react';
-
-import sanDiego from '../images/san-diego.jpg';
+import Measure              from 'react-measure';
 
 class Image extends Component {
+  state = {
+    dimensions: {
+      width: -1,
+      height: -1
+    },
+    hovering: false
+  }
+
+  handleHover(hover) {
+    this.setState({hovering: hover});
+  }
+
   render() {
+    const { width, height } = this.state.dimensions;
+    const { hovering } = this.state;
+
     return (
-      <img
-        className='image'
-        src={sanDiego}
-      />
+      <Measure
+        bounds
+        onResize={(contentRect) => {
+          this.setState({ dimensions: contentRect.bounds })
+        }}
+      >
+        {({ measureRef }) =>
+          <div
+            ref={measureRef}
+            className='grid-image'
+            style={{
+              backgroundImage: `url(${this.props.src})`,
+              backgroundPosition: 'center center',
+              backgroundSize: 'cover',
+              height: width
+            }}
+            onMouseOver={() => this.handleHover(true)}
+            onMouseLeave={() => this.handleHover(false)}
+          >
+            <div
+              className='grid-image--overlay'
+              style={{
+                height: width,
+                width: width
+              }}
+            >
+              { hovering &&
+                <div>Damn, what a great photo</div>
+              }
+            </div>
+          </div>
+        }
+      </Measure>
     );
   }
 }
